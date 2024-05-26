f0adc4c536711562e280a83c977b0fde33138309
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-05-26 16:07:44 +0200

    chore: Add footer, hero section, and more links to home page

---
+++ src/lib/home/footer.svelte
@@ -0,0 +1,12 @@
+<footer
+	class="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6"
+>
+	<p class="text-xs text-gray-500">© {new Date().getUTCFullYear()} ZERO. All rights reserved.</p>
+	<nav class="flex gap-4 sm:ml-auto sm:gap-6">
+		<a class="text-xs underline-offset-4 hover:underline" href="/github"> GitHub </a>
+		<a class="text-xs underline-offset-4 hover:underline" href="/api"> API'S </a>
+		<a class="text-xs underline-offset-4 hover:underline" href="mailto:hello@synthetic.me">
+			Contact
+		</a>
+	</nav>
+</footer>
---
+++ src/lib/home/skills.svelte
@@ -0,0 +1,187 @@
+<section id="skills" class=" flex w-full justify-center py-12 md:py-24 lg:py-32">
+	<div class="container px-4 md:px-6">
+		<div class="cc flex flex-col items-center space-y-4 text-center">
+			<div class="space-y-2">
+				<h2 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Skills</h2>
+				<p
+					class="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+				>
+					Here are some of the technologies I'm proficient in.
+				</p>
+			</div>
+		</div>
+		<div class="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-3 lg:gap-12">
+			<div class="grid gap-4">
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Programming Languages</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-green-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Node.js
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Typescript
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-orange-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Rust
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Python
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							C++
+						</div>
+					</div>
+				</div>
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Databases</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-green-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							MongoDB
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							PostgreSQL
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-red-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Redis
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Pocketbase
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="grid gap-4">
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Frameworks &amp; Libraries</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-orange-400/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							tensorflow
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							FastAPI
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-orange-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							PyTorch
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-orange-600/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Svelte Kit
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-purple-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Astro
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Next.js
+						</div>
+					</div>
+				</div>
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Infrastructure</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Docker
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Kubernetes
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-yellow-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							AWS
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Vercel
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="grid gap-4">
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Methodologies</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="focus:ring-ring bg-secondary hover:bg-secondary/80 inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Agile
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary hover:bg-secondary/80 inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							TDD
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary hover:bg-secondary/80 inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Microservices
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary hover:bg-secondary/80 inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Supervised Learning
+						</div>
+					</div>
+				</div>
+				<div class="grid gap-1">
+					<h3 class="text-lg font-bold">Tools</h3>
+					<div class="flex flex-wrap gap-2">
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-orange-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Git
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							GitHub
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-blue-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Vscode
+						</div>
+						<div
+							class="focus:ring-ring bg-secondary inline-flex w-fit items-center whitespace-nowrap rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors hover:bg-purple-500/80 focus:outline-none focus:ring-2 focus:ring-offset-2"
+						>
+							Insomnia
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
