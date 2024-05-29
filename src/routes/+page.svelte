7aca639ee337a40023f65580907f9d62fb4ee556
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-05-29 22:17:45 +0200

    feat: Update landing page components and routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,23 @@
 <script lang="ts">
-	import Header from '$lib/header.svelte';
-	import Footer from '$lib/home/footer.svelte';
-	import Morelinks from '$lib/home/morelinks.svelte';
-	import Skills from '$lib/home/skills.svelte';
-	import Featuredprojects from '$lib/home/featuredprojects.svelte';
-	import Herosection from '$lib/home/herosection.svelte';
+	import Header from '$lib/ui/header.svelte';
+	import Footer from '$lib/ui/footer.svelte';
+	import Morelinks from '$lib/ui/landingPage/section_rountes.svelte';
+	import Skills from '$lib/ui/landingPage/section_skills.svelte';
+	import Herosection from '$lib/ui/landingPage/section_hero.svelte';
+	import Lastprojects from '$lib/ui/landingPage/seaction_lastprojects.svelte';
 
 	export let data;
+	console.log(data);
 </script>
 
 <svelte:head>
 	<title>Home | ZERO</title>
-	<meta
-		name="description"
-		content="ZERO is a backend developer and AI engineer with a passion for building scalable and efficient web applications and artificial intelligence. synthetix.me"
-	/>
-	<meta
-		name="keywords"
-		content="backend developer, ai engineer, web applications, artificial intelligence, synthetix.me, Zero, ZERO,tschome, tsc-home, TSC-HOME, synthetix, planty"
-	/>
-	<meta name="author" content="ZERO" />
-	<meta name="robots" content="index, follow" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 <div class="flex min-h-[100dvh] flex-col">
 	<Header {data} />
 	<main class="flex-1">
 		<Herosection />
-		<Featuredprojects />
+		<Lastprojects projects={data.projects} />
 		<Skills />
 		<Morelinks />
 	</main>
