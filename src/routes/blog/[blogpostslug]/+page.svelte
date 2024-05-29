e378d92fd4b339441061df0e8534bd887839d535
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-05-29 22:18:36 +0200

    refactor: Update blog page to use UI components for header, footer, and blog post cards

---
+++ src/routes/blog/[blogpostslug]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Header from '$lib/ui/header.svelte';
+	import { formatDate } from '$lib/utils';
+	export let data;
+	const { content, frontmatter } = data.blogpost;
+</script>
+
+<div>
+	<Header {data} />
+</div>
+<article class="prose p-28">
+	<header>
+		<h1 class="title">{frontmatter.title}</h1>
+		<p class="published">Published {formatDate(frontmatter.published)}</p>
+	</header>
+
+	{@html content}
+</article>
