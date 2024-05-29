6729e734b12739c75cc9bd0453811da8481c310b
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-05-29 22:17:13 +0200

    feat: Add section for more links on landing page

---
+++ src/lib/ui/landingPage/section_rountes.svelte
@@ -0,0 +1,64 @@
+<section id="more" class=" flex w-full justify-center bg-gray-100 py-12 md:py-24 lg:py-32">
+	<div class="container px-4 md:px-6">
+		<div class="flex flex-col items-center justify-center space-y-4 text-center">
+			<div class="space-y-2">
+				<h2 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">More Links</h2>
+				<p
+					class="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+				>
+					Check all of my suff out. I'm sure you'll find something interesting.
+				</p>
+			</div>
+		</div>
+		<div class="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+			<div class=" rounded-lg border bg-white shadow-sm duration-700 hover:scale-105">
+				<div class="flex flex-row items-center gap-4 space-y-1.5 p-6">
+					<div class="grid gap-1">
+						<h3 class="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">
+							Blog
+						</h3>
+						<p class=" text-sm">A little tech block.</p>
+					</div>
+				</div>
+				<div class="grid gap-2 p-6">
+					<p class="text-sm text-gray-500">
+						My blog is a platform for me to share my interests and thoughts on various topics. I
+						hope you will find something of interest on my blog.
+					</p>
+					<div class="flex items-center gap-4 text-sm">
+						<a
+							class="inline-flex h-8 items-center justify-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
+							href="/blog"
+						>
+							View My Blog
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class=" rounded-lg border bg-white shadow-sm duration-700 hover:scale-105">
+				<div class="flex flex-row items-center gap-4 space-y-1.5 p-6">
+					<div class="grid gap-1">
+						<h3 class="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">
+							Portfolio
+						</h3>
+						<p class=" text-sm">A list off all my public projects.</p>
+					</div>
+				</div>
+				<div class="grid gap-2 p-6">
+					<p class="text-sm text-gray-500">
+						I've put together a list of all my projects, both published and in progress. If you're
+						interested in seeing more, please feel free to drop by.
+					</p>
+					<div class="flex items-center gap-4 text-sm">
+						<a
+							class="inline-flex h-8 items-center justify-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
+							href="/projects"
+						>
+							View My Portfolio
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
