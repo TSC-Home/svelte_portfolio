e1d0036279ef29cc812cd5b72fdfc48d449df63b
ZERO <103266275+TSC-Home@users.noreply.github.com>
2024-05-29 22:18:07 +0200

    refactor: Update blog page to use UI components for header, footer, and blog post cards

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,80 +1,14 @@
 <script lang="ts">
-	import Header from '$lib/header.svelte';
-	import Footer from '$lib/home/footer.svelte';
-	import Card from '$lib/projects/Card.svelte';
+	import Header from '$lib/ui/header.svelte';
+	import Footer from '$lib/ui/footer.svelte';
+	import Card from '$lib/ui/blog_posts_cards.svelte';
+	import ErrorCard from '$lib/ui/error_card.svelte';
 
 	export let data;
-
-	let projects = [
-		{
-			title: 'Basics of LLM',
-			description: 'How does a language model work?',
-			info: 'A little blog post about the basics of a Language Model.',
-			name: 'basics-of-llm',
-			icon: 'grain',
-			my: false,
-			status: 'Artificial Intelligence',
-			linkname: '',
-			link: '',
-			direct_redirect: false,
-			readmore: true
-		},
-		{
-			title: 'Person detection model',
-			description: 'How to build a person detection model with OpenCV and Python.',
-			info: 'A little blog post about how to build a person detection model with OpenCV and Python.',
-			name: 'how-to-build-a-person-detection-model',
-			icon: 'person_search',
-			my: false,
-			status: 'Artificial Intelligence',
-			link: '/projects/how-to-build-a-person-detection-model',
-			linkname: 'Detection model',
-			direct_redirect: false,
-			readmore: true
-		},
-		{
-			title: 'Privacy',
-			description: 'Why is privacy important?',
-			info: 'A little blog post about why privacy is important. And how you can protect your privacy.',
-			name: 'privacy',
-			icon: 'privacy_tip',
-			my: false,
-			status: 'Privacy',
-			link: '',
-			linkname: 'Privacy',
-			direct_redirect: false,
-			readmore: true
-		},
-		{
-			title: 'How to code Green?',
-			description: 'How to programme more sustainably.',
-			info: 'A little blog post about how to programme more sustainably. And how you can reduce your carbon footprint.',
-			name: 'how-to-code-green',
-			icon: 'eco',
-			my: false,
-			status: 'Sustainability',
-			link: '',
-			linkname: 'Sustainability',
-			direct_redirect: false,
-			readmore: true
-		}
-	];
 </script>
 
 <svelte:head>
 	<title>Blog | ZERO</title>
-	<meta
-		name="description"
-		content="ZERO is a backend developer and AI engineer with a passion for building scalable and efficient web applications and artificial intelligence. synthetix.me"
-	/>
-	<meta
-		name="keywords"
-		content="backend developer, ai engineer, web applications, artificial intelligence, synthetix.me, Zero, ZERO,tschome, tsc-home, TSC-HOME, synthetix, planty, projects, portfolio, work, experience, contact, contact me, contact form, email, email me"
-	/>
-	<meta name="author" content="ZERO" />
-	<meta name="robots" content="index, follow" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 <div class="flex min-h-[100dvh] flex-col">
 	<Header {data} />
@@ -93,9 +27,15 @@
 					</div>
 				</div>
 				<div class="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-					{#each projects as project, i}
-						<Card projects={projects[i]} />
-					{/each}
+					{#if data.posts}
+						{#each data.posts as posts}
+							<Card projects={posts} />
+						{/each}
+					{:else}
+						<div>
+							<ErrorCard />
+						</div>
+					{/if}
 				</div>
 			</div>
 		</section>
